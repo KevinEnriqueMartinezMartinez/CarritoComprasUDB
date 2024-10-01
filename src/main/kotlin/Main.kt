8990346802748986c86e1b6ba2169bec80ab8692
KevinEnriqueMartinezMartinez <127909032+KevinEnriqueMartinezMartinez@users.noreply.github.com>
2024-09-30 22:01:11 -0600

    Opciones del Menu
    se creo un do while donde se imprime el menu y se hizo un do while donde dependiendo la opcion del usuario con un case entra y ejecuta esa opcion
    
    Co-Authored-By: SofiaRojas2890 <129335267+SofiaRojas2890@users.noreply.github.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,48 @@ fun main() {
 
 
 
+    val carrito = CarritoDeCompras()
+    var opcion: Int
+
+    do {
+        println("\n--- UDB TECHNOLOGY S.A de C.V ---")
+        println("1. Ver productos")
+        println("2. Agregar producto al carrito")
+        println("3. Ver carrito")
+        println("4. Eliminar producto del carrito")
+        println("5. Finalizar compra")
+        println("0. Salir")
+        print("Elija una opción: ")
+        opcion = readLine()?.toIntOrNull() ?: 0
+
+        when (opcion) {
+            1 -> {
+                println("\nProductos disponibles:")
+                productosDisponibles.forEach { producto ->
+                    println("${producto.id}. ${producto.nombre} - $${producto.precio} (Cantidad disponible: ${producto.cantidadDisponible})")
+                }
+            }
+            2 -> {
+                print("Ingrese el ID del producto: ")
+                val idProducto = readLine()?.toIntOrNull() ?: 0
+                val productoSeleccionado = productosDisponibles.find { it.id == idProducto }
+
+                if (productoSeleccionado != null) {
+                    print("Ingrese la cantidad: ")
+                    val cantidad = readLine()?.toIntOrNull() ?: 1
+                    carrito.agregarProducto(productoSeleccionado, cantidad)
+                } else {
+                    println("Producto no encontrado.")
+                }
+            }
+
+
+
+
+
+
+
+
 
 
 }
